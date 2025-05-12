71d47151c6231149331202e6d80e31060f68640d
qsrdev <matteo.albertini747@gmail.com>
2025-05-12 16:02:25 +0200

    feat: code done fixes incoming

--- js/script.js
+++ js/script.js
@@ -2,19 +2,25 @@ const apiUrl = "https://lanciweb.github.io/demo/api/pictures/";
 let card = document.querySelector(".card");
 
 axios.get(apiUrl).then((resp) => {
-  const cardtemplate = "";
   const posts = resp.data;
-  console.log(resp.data);
+  console.log(posts);
 
+  //funzione che scorre l'array di oggetti e che destruttura ogni singolo oggetto ritornado
+  // titolo data e url dell'immagine
+  let cardTemplate = "";
   posts.forEach((singlePost) => {
+    console.log(singlePost);
     const { title, date, url } = singlePost;
     console.log(title, date, url);
-    card.innerHTML = `
-    <img src="./img/pin.svg" />
-                            <img src="${url}" alt="${title}" width="200" height="200">
-                             <p>${date}</p>
-                             <h3>${title}</h3>
-                        
-                    `;
+    cardTemplate += `   <div class="imgcontainer">
+                            <img src="${url}" alt="${title}" width="300" height="300" />
+                            <img class="pin" src="./img/pin.svg" />
+                        </div>
+                        <div class="text">
+                            <p>"${date}"</p>
+                            <h3>"${title}"</h3>
+                        </div>`;
+    console.log(cardTemplate);
   });
+  card.innerHTML = cardTemplate;
 });
