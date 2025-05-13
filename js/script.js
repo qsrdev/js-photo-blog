a90a68e6242ca92fdb39c82ad068d8e08ae53a68
qsrdev <matteo.albertini747@gmail.com>
2025-05-13 15:01:19 +0200

    feat: refactor + comments

--- js/script.js
+++ js/script.js
@@ -1,31 +1,45 @@
 const apiUrl = "https://lanciweb.github.io/demo/api/pictures/";
-let card = document.querySelector(".row");
 
+//axios è una libreria che serve per effettuare richieste in HTTP
+//la prima parte fa la richiesta ad apiURL
+//then viene eseguito soltanto se la richiesta ha successo
 axios.get(apiUrl).then((resp) => {
+  //assegnamo a post la risposta dell'API - .data contiene il corpo della nostra risposta
+  // in questo caso l'array contenente gli oggetti e lo chiamiamo posts
   const posts = resp.data;
-  console.log(posts);
-
-  //funzione che scorre l'array di oggetti e che destruttura ogni singolo oggetto ritornado
-  // titolo data e url dell'immagine
-  let cardTemplate = "";
-  posts.forEach((singlePost) => {
-    console.log(singlePost);
-    const { title, date, url } = singlePost;
-    console.log(title, date, url);
-    cardTemplate += `<div class="col">
-          <div class="card">
-            <div class="imgcontainer">
-              <img class="pin" src="./img/pin.svg" />
-              <img id="open" src="${url}" alt="${title}" />
-            </div>
-            <div class="text">
-              <p>${date}</p>
-              <h2>${title}</h2>
-            </div>
-          </div>
-        </div>`;
-
-    console.log(cardTemplate);
-  });
-  card.innerHTML = cardTemplate;
+  // console.log(posts);
+  postPrinter(posts);
 });
+
+function postPrinter(arr) {
+  //prendo row element con il dom
+  const rowElem = document.querySelector(".row");
+  //creo la variabile vuota dove vado andrò concatenare postTemplate che ha ciclato
+  //gli object dell'array e quindi crea l'html pronto
+  let cardTemplate = "";
+  // arr = posts in questo caso - scorriamo ogni oggetto dell'array posts
+  //postTemplate destruttura l'oggetto e inserisce i parametri dove mi servono
+  //siccome postTemplate ritorna HTML posso concatenare dentro cardTemplate per avere
+  //il codice pronto
+  arr.forEach((singlePost) => (cardTemplate += postTemplate(singlePost)));
+  //printo tramite API del dom con innerHTML
+  rowElem.innerHTML = cardTemplate;
+}
+
+// destrutturiamo ogni oggetto che viene passato dentro questa funzione
+// ritorna un HTML con diversi parametri inseriti nel testo
+function postTemplate(post) {
+  const { title, date, url } = post;
+  return `<div class="col">
+            <div class="card">
+              <div class="imgcontainer">
+                <img class="pin" src="./img/pin.svg" />
+                <img id="open" src="${url}" alt="${title}" />
+              </div>
+              <div class="text">
+                <p>${date}</p>
+                <h2>${title}</h2>
+              </div>
+            </div>
+          </div>`;
+}
